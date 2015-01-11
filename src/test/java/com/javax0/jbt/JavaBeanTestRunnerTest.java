e2da02db924e0d80b89bc43da436a76f3b682a0b
Peter Verhas <peter@verhas.com>
2015-01-11 15:45:07 +0100

    test runner test

--- src/test/java/com/javax0/jbt/JavaBeanTestRunnerTest.java
+++ src/test/java/com/javax0/jbt/JavaBeanTestRunnerTest.java
@@ -1,8 +1,132 @@
 package com.javax0.jbt;
 
-import org.junit.runner.RunWith;
+import static com.javax0.jbt.Mock.forClass;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
-@RunWith(JavaBeanTestRunner.class)
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.mockito.Mockito;
+
+import com.javax0.jbt.testedbeans.AlternateGetterTest;
+import com.javax0.jbt.testedbeans.CorrectBeanTest;
+import com.javax0.jbt.testedbeans.EmptyTest;
+import com.javax0.jbt.testedbeans.FakeTestClass;
+import com.javax0.jbt.testedbeans.NoGetterTest;
+import com.javax0.jbt.testedbeans.NoSetterTest;
+import com.javax0.jbt.testedbeans.NonOrthogonalFieldsTest;
+import com.javax0.jbt.testedbeans.ObjectFieldsTest;
+import com.javax0.jbt.testedbeans.PrimitiveFieldsTest;
+
 public class JavaBeanTestRunnerTest {
 
+	private void assertRunWasSuccessful() {
+		verify(notifier, Mockito.atLeast(1)).fireTestRunStarted(
+				any(Description.class));
+		verify(notifier, never()).fireTestFailure(any(Failure.class));
+		verify(notifier, Mockito.atLeast(1)).fireTestFinished(
+				any(Description.class));
+	}
+
+	private void assertRunsWereSuccessful() {
+		verify(notifier, Mockito.atLeast(1)).fireTestRunStarted(
+				any(Description.class));
+		verify(notifier, never()).fireTestFailure(any(Failure.class));
+		verify(notifier, Mockito.atLeast(1)).fireTestFinished(
+				any(Description.class));
+	}
+
+	private void assertRunFailed() {
+		verify(notifier).fireTestRunStarted(any(Description.class));
+		verify(notifier).fireTestFailure(any(Failure.class));
+		verify(notifier).fireTestFinished(any(Description.class));
+	}
+
+	private void assertRunFailed(int times) {
+		verify(notifier, Mockito.atLeast(1)).fireTestRunStarted(
+				any(Description.class));
+		verify(notifier, Mockito.atLeast(times)).fireTestFailure(
+				any(Failure.class));
+		verify(notifier, Mockito.atLeast(1)).fireTestFinished(
+				any(Description.class));
+	}
+
+	private void assertNoTestWasStarted() {
+		verify(notifier, never()).fireTestRunStarted(any(Description.class));
+		verify(notifier, never()).fireTestFailure(any(Failure.class));
+		verify(notifier, never()).fireTestFinished(any(Description.class));
+	}
+
+	private Runner runner;
+	private RunNotifier notifier;
+
+	private void setupRunner(Class<? extends FakeTestClass> testClass) {
+		runner = new JavaBeanTestRunner(testClass);
+	}
+
+	@Before
+	public void setupNotifier() {
+		notifier = (RunNotifier) forClass(RunNotifier.class);
+	}
+
+	@Test
+	public void testsCorrectBean() {
+		setupRunner(CorrectBeanTest.class);
+		runner.run(notifier);
+		assertRunWasSuccessful();
+	}
+
+	@Test
+	public void alternateGetterBean() {
+		setupRunner(AlternateGetterTest.class);
+		runner.run(notifier);
+		assertRunWasSuccessful();
+	}
+
+	@Test
+	public void primitiveFields() {
+		setupRunner(PrimitiveFieldsTest.class);
+		runner.run(notifier);
+		assertRunsWereSuccessful();
+	}
+
+	@Test
+	public void objectFields() {
+		setupRunner(ObjectFieldsTest.class);
+		runner.run(notifier);
+		assertRunsWereSuccessful();
+	}
+
+	@Test
+	public void testsEmptyBean() {
+		setupRunner(EmptyTest.class);
+		runner.run(notifier);
+		assertNoTestWasStarted();
+	}
+
+	@Test
+	public void noGetterBeanFails() {
+		setupRunner(NoGetterTest.class);
+		runner.run(notifier);
+		assertRunFailed();
+	}
+
+	@Test
+	public void noSetterBeanFails() {
+		setupRunner(NoSetterTest.class);
+		runner.run(notifier);
+		assertRunFailed();
+	}
+
+	@Test
+	public void nonOrthogonalFieldsBeanFails() {
+		setupRunner(NonOrthogonalFieldsTest.class);
+		runner.run(notifier);
+		assertRunFailed(NonOrthogonalFieldsTest.NUMBER_OF_NONORTHOGONAL_FIELDS);
+	}
 }
--- src/test/java/com/javax0/jbt/StandardNamedBeanTest.java
+++ src/test/java/com/javax0/jbt/StandardNamedBeanTest.java
@@ -2,7 +2,7 @@ package com.javax0.jbt;
 
 import org.junit.runner.RunWith;
 
-@RunWith(JavaBeanTestRunner.class)
+//@RunWith(JavaBeanTestRunner.class)
 public class StandardNamedBeanTest {
 
 }
